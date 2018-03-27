a41ab0453b41bd8edb6734bd572a791c7605f0eb
David Boyd <dave@intuitioneducation.com.au>
2018-03-27 22:17:18 +1100

    deploying to heroku cleanup (remove database)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,8 +7,6 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
-ActiveRecord::Migration.maintain_test_schema!
-
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   #config.fixture_path = "#{::Rails.root}/spec/fixtures"
