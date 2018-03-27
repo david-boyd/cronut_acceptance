3852fe97e8014737eee8f99f63911262291c6306
David Boyd <dave@intuitioneducation.com.au>
2018-03-27 22:00:35 +1100

    add comments to validator

--- app/models/external_url.rb
+++ app/models/external_url.rb
@@ -1,3 +1,7 @@
+# Helper model to validate urls. Makes use of the validate_url gem.
+# Urls must be fully formed
+# e.g.  http://www.google.com or https://www.google.com are valid
+#       www.google.com is NOT valid
 class ExternalUrl
   include ActiveModel::Validations
 
